7e3dd3707022dd967479c5f0b880d088edb36147
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2019-03-16 13:51:30 +0100

    Generate one voucher at a time

--- voucher_generator.py
+++ voucher_generator.py
@@ -1,12 +1,12 @@
-#Generating a voucher
+# Generating a voucher
 
 import numpy as np
 
-class voucher():
 
-    def __init__(self, type):
+class Voucher:
+    def __init__(self, voucher_type):
         self.value = 0.0
-        self.type = type
+        self.type = voucher_type
         self.percentage = 0.0
         self.max_order_value = 0.0
         self.with_money = False
@@ -23,46 +23,47 @@ class voucher():
         self.max_order_value = max_order_value
 
     def print(self):
-        print('Type: '+self.type + ' Percent: '+str(self.percentage) + ' Value: '+str(self.value) + ' Max order: ' + str(self.max_order_value))
-
+        print('Type: ' + self.type + ' Percent: ' + str(self.percentage) + ' Value: ' + str(
+            self.value) + ' Max order: ' + str(self.max_order_value))
 
 
 def predicted_no_of_issues(mean_no_of_issues, stddev_no_of_issues):
     no_of_issues = int(round(np.random.normal(mean_no_of_issues, stddev_no_of_issues)))
     return no_of_issues
 
-def randomizer_generator(expected_no_of_issues, types_of_vouchers, budget, min_voucher_value):
 
-    vector_of_types = np.random.randint(len(types_of_vouchers), size = expected_no_of_issues)
+def random_generator(expected_no_of_issues, types_of_vouchers, budget, min_voucher_value):
+    vector_of_types = np.random.randint(len(types_of_vouchers), size=expected_no_of_issues)
 
-    expected_no_of_issues_solved_with_money = int(round(expected_no_of_issues / 2))
+    expected_no_of_issues_solved_with_money = int(round(expected_no_of_issues))
     expected_mean_voucher_value = budget / expected_no_of_issues_solved_with_money
-    exponential_distribution_coef =  expected_mean_voucher_value - min_voucher_value
+    exponential_distribution_coef = expected_mean_voucher_value - min_voucher_value
 
     if exponential_distribution_coef <= 0:
         print('Budget is too small!')
 
-    vector_of_values = np.random.exponential(exponential_distribution_coef, size = expected_no_of_issues)
+    vector_of_values = np.random.exponential(exponential_distribution_coef, size=expected_no_of_issues)
 
-    vector_of_percentages = 0.05 * np.random.randint(1, 11, size = expected_no_of_issues)
+    vector_of_percentages = 0.05 * np.random.randint(1, 11, size=expected_no_of_issues)
 
-    return (vector_of_types, vector_of_values, vector_of_percentages)
+    return vector_of_types, vector_of_values, vector_of_percentages
 
 
 def generate_vouchers(budget, expected_no_of_issues, types_of_vouchers, min_voucher_value, max_voucher_value):
-
-    (vector_of_types, vector_of_values, vector_of_percentages) = randomizer_generator(expected_no_of_issues, types_of_vouchers, budget, min_voucher_value)
+    (vector_of_types, vector_of_values, vector_of_percentages) = random_generator(expected_no_of_issues,
+                                                                                  types_of_vouchers, budget,
+                                                                                  min_voucher_value)
 
     voucher_list = []
 
     for issue_index in range(expected_no_of_issues):
 
-        voucher_instance = voucher(types_of_vouchers[vector_of_types[issue_index]])
+        voucher_instance = Voucher(types_of_vouchers[vector_of_types[issue_index]])
 
         if voucher_instance.type == 'percentage':
             voucher_value = round(min(vector_of_values[issue_index] + min_voucher_value, max_voucher_value), 2)
             voucher_percentage = vector_of_percentages[issue_index]
-            max_order_value = round(voucher_value / voucher_percentage , 2)
+            max_order_value = round(voucher_value / voucher_percentage, 2)
 
             voucher_instance.add_value(voucher_value)
             voucher_instance.add_percentage(voucher_percentage)
@@ -82,25 +83,56 @@ def generate_vouchers(budget, expected_no_of_issues, types_of_vouchers, min_vouc
 
     return voucher_list
 
-def test():
-    mean_no_of_issues = 200
-    stddev_no_of_issues = 20
+
+def get_voucher(budget, expected_no_of_issues, types_of_vouchers, min_voucher_value, max_voucher_value):
+    expected_no_of_issues_solved_with_money = int(round(expected_no_of_issues))
+    expected_mean_voucher_value = budget / expected_no_of_issues_solved_with_money
+    exponential_distribution_coef = 2 * expected_mean_voucher_value - min_voucher_value
+    voucher_type = np.random.randint(len(types_of_vouchers))
+    voucher_value = np.random.exponential(exponential_distribution_coef)
+    voucher_value = round(min(voucher_value + min_voucher_value, max_voucher_value), 2)
+    voucher_percentage = 0.01 * np.random.randint(1, 6)
+    voucher_instance = Voucher(types_of_vouchers[voucher_type])
+
+    if voucher_instance.type == 'percentage':
+        max_order_value = round(voucher_value / voucher_percentage, 2)
+        voucher_instance.add_value(voucher_value)
+        voucher_instance.add_percentage(voucher_percentage)
+        voucher_instance.add_max_order_value(max_order_value)
+
+    elif voucher_instance.type == 'value':
+        voucher_instance.add_value(voucher_value)
+
+    return voucher_instance
+
+
+def test_batch():
+    mean_no_of_issues = 3000
+    stddev_no_of_issues = 0
     types_of_vouchers = ['value', 'percentage', 'apology', 'free delivery']
-    min_voucher_value = 5.0
-    max_voucher_value = 20.0
-    budget = 2000.0
+    min_voucher_value = 1.0
+    max_voucher_value = 10.0
+    budget = 10000.0
     achieved_budget = 0.0
+    budget_spent = 0
+    incidents = 0
 
     expected_no_of_issues = predicted_no_of_issues(mean_no_of_issues, stddev_no_of_issues)
-    voucher_list = generate_vouchers(budget, expected_no_of_issues, types_of_vouchers, min_voucher_value, max_voucher_value)
+    voucher_list = generate_vouchers(budget, expected_no_of_issues, types_of_vouchers, min_voucher_value,
+                                     max_voucher_value)
 
     for issue_index in range(expected_no_of_issues):
-        voucher_instance = voucher_list[issue_index]
+        voucher_instance = get_voucher(budget - budget_spent, expected_no_of_issues - incidents,
+                                       types_of_vouchers, min_voucher_value,
+                                       max_voucher_value)
         voucher_instance.print()
         achieved_budget += voucher_instance.value
+        budget_spent += voucher_instance.value
+        incidents += 1
 
-    print('Number of issues: ' + str(expected_no_of_issues) +' Achieved budget:' + str(achieved_budget))
+    print('Number of issues: ' + str(expected_no_of_issues) + ' Achieved budget:' + str(achieved_budget))
 
     return 1
 
-test()
+
+test_batch()
